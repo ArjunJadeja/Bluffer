20bd36d448795a0aa4a13a4373c6d451b0747c2c
ArjunJadeja <contactarjunjadeja@gmail.com>
2022-06-02 23:33:56 +0530

    Initial commit

---
+++ app/src/main/java/com/arjun/bluffer/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.arjun.bluffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}

b88f28e8bc7e429c9fabe95457c0d992c26dc57c
ArjunJadeja <contactarjunjadeja@gmail.com>
2022-07-03 02:58:49 +0530

    Github Commit

--- app/src/main/java/com/arjun/bluffer/view/PlayFragment.kt
+++ app/src/main/java/com/arjun/bluffer/view/PlayFragment.kt
@@ -9,11 +9,13 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import com.arjun.bluffer.R
 import com.arjun.bluffer.databinding.FragmentPlayBinding
+import com.arjun.bluffer.utils.Helper
 import com.arjun.bluffer.viewmodel.SharedViewModel
 
 class PlayFragment : Fragment(R.layout.fragment_play) {
 
     private lateinit var binding: FragmentPlayBinding
+    private val rules: Helper = Helper()
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -27,15 +29,42 @@ class PlayFragment : Fragment(R.layout.fragment_play) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPlayBinding.bind(view)
 
+        binding.aboutGame.text = rules.aboutGame
+        binding.rulesList.text = rules.rulesList
+
         binding.playButton.setOnClickListener {
             binding.playButton.visibility = View.GONE
+            binding.helpButton.visibility = View.GONE
             binding.playerNamesCardView.visibility = View.VISIBLE
         }
         binding.nextButton.setOnClickListener {
-            sharedViewModel.playersName(binding.playerOneName.editText!!.text.toString(), binding.playerTwoName.editText!!.text.toString())
+//            if (checkNames()) {
+                sharedViewModel.playersName(
+                    binding.playerOneName.editText!!.text.toString(),
+                    binding.playerTwoName.editText!!.text.toString()
+                )
+//            } else {
+//                Toast.makeText(activity, "Enter valid names!", Toast.LENGTH_SHORT).show()
+//            }
             binding.playerNamesCardView.visibility = View.GONE
             Navigation.findNavController(view).navigate(R.id.action_playFragment_to_gameFragment)
         }
+        binding.helpButton.setOnClickListener {
+            if (binding.helperCardView.visibility == View.GONE) {
+                binding.playButton.visibility = View.GONE
+                binding.helperCardView.visibility = View.VISIBLE
+            } else {
+                binding.helperCardView.visibility = View.GONE
+                binding.playButton.visibility = View.VISIBLE
+            }
+        }
+        binding.closeButton.setOnClickListener {
+            binding.helperCardView.visibility = View.GONE
+            binding.playButton.visibility = View.VISIBLE
+        }
     }
+//    private fun checkNames(): Boolean {
+//        return binding.playerOneName.editText != null && binding.playerTwoName.editText != null
+//    }
 
 }
